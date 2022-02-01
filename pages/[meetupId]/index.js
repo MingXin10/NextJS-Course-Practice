d5a8d39b4cdbfabf3d9ba5bc592fe538f3152272
Joey <smallstars10@gmail.com>
2022-02-01 12:05:09 +0800

    修正fallback

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -31,7 +31,7 @@ export async function getStaticPaths() {
   return {
     //fallback為fall=>只會render paths裡面有的內容，如果使用者直接key paths沒有的，會得到404。可以用來設定使用者較常造訪的頁面
     //fallback為true=>會從server找符合的資料。設定較少造訪的，就不會造成太多request
-    fallback: false,
+    fallback: 'blocking',
     paths: meetups.map((meetup) => ({
       params: {
         meetupId: meetup._id.toString()
